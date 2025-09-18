596b0bc57a730afb70f4372bbbb5e52efca2ab11
xuetong_wang <xuetong_wang@intsig.net>
2025-09-18 18:38:28 +0800

    feat: bill docs

---
+++ global.js
@@ -0,0 +1,9 @@
+var pathname = window.location.pathname;
+
+// 依据路由修改快速体验按钮跳转链接
+if (pathname.startsWith("/xparse")) {
+  var primaryButton = document.querySelector("#topbar-cta-button > a");
+  if (primaryButton) {
+    primaryButton.setAttribute("href", "https://www.textin.com/console/recognition/robot_bills?service=bill_recognize_v2");
+  }
+}
